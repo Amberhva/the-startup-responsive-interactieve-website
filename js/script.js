77342df953ae5e49f2256e77c1e0119074ad58fa
Amberhva <amber.van.der.reijden@hva.nl>
2023-01-13 10:02:40 +0100

    Update
    Added a sticky nav and custom properties for the setup.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let gap = 50;
+
+$(document).ready(function(){
+    $(window).bind('scroll', function(){
+        if ($(window).scrollTop() > gap){
+            $('nav').addClass('active');
+        } else {
+            $('nav').removeClass('active');
+        }
+    });
+});
