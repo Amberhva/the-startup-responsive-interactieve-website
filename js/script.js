625dfbb49a15140845720fed3e003de90f010efe
Amberhva <amber.van.der.reijden@hva.nl>
2023-01-25 20:59:29 +0100

    Update
    Changes to the scroll animation

--- js/script.js
+++ js/script.js
@@ -26,34 +26,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-
-
-// Scroll animation for the first & last fact
-const observerCards = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show-cards');
-        } else {
-            entry.target.classList.remove('show-cards');
-        }
-    });
-});
-
-const hiddenCardsElements = document.querySelectorAll('.hidden-cards');
-hiddenCardsElements.forEach((el) => observerCards.observe(el));
-
-// Scroll animation for the second fact
-const observerSecondCard = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show-second-card');
-        } else {
-            entry.target.classList.remove('show-second-card');
-        }
-    });
-});
-
-const hiddenSecondCardElements = document.querySelectorAll('.hidden-second-card');
-hiddenSecondCardElements.forEach((el) => observerSecondCard.observe(el));
